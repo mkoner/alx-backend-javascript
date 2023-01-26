8994249da0c672c69db82ec87791102ea622ecf2
mkoner <amzid@hotmail.fr>
2023-01-26 22:49:44 +0000

    Task5 again

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,10 @@
 export default class Building {
     constructor(sqft) {
-        if (this.constructor === 'Building')
-            throw new Error("Cannot instantiate abstract class")
-        if (typeof evacuationWarningMessage !== 'function')
-            throw new Error("Class extending Building must override evacuationWarningMessage");
-	this._sqft = sqft;
+        this._sqft = sqft;
+        if (this.constructor !== Building) {
+            if (typeof evacuationWarningMessage !== 'function')
+                throw new Error("Class extending Building must override evacuationWarningMessage");
+        }
     }
 
     get sqft() {
