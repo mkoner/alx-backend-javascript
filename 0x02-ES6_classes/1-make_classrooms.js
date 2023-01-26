4642c6b718682511ed35fda8d8ed4dadae95a32c
mkoner <amzid@hotmail.fr>
2023-01-26 14:28:29 +0000

    Task1 again

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,2 +1,2 @@
 import ClassRoom from './0-classroom';
-export default const initializeRooms = [19, 20, 34].map(number => new Classroom(number)); 
+export const initializeRooms = [19, 20, 34].map(number => new Classroom(number)); 
