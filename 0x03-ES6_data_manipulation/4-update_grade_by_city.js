96693a76f50a15e6872361e3e192f55c3bae9f34
mkoner <amzid@hotmail.fr>
2023-01-31 12:50:51 +0000

    Task4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,13 @@
+/* eslint-disable no-param-reassign */
 export default function updateStudentGradeByCity(list, city, newGrades) {
-  const studentsToUpdate = Array.isArray(list) ? list.filter((student) => student.location === city) : [];
+  const studentsToUpdate = list.filter((student) => student.location === city);
   return studentsToUpdate.map((student) => {
-    const grade = Array.isArray(newGrades) ? newGrades.filter((gra) => gra.studentId === student.id) : null;
-    grade.length === 1 ? student.grade = grade[0].grade : student.grade = 'N/A';
+    const grade = newGrades.filter((gra) => gra.studentId === student.id);
+    if (grade.length === 1) {
+      student.grade = grade[0].grade;
+    } else {
+      student.grade = 'N/A';
+    }
     return student;
   });
 }
