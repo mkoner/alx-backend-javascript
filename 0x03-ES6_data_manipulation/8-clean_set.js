260f2c6ffc658323aab016f07a8e942b232764e6
mkoner <amzid@hotmail.fr>
2023-01-31 16:30:23 +0000

    Task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (typeof (startString) !== 'string') return null;
   const strings = [];
   for (const item of set) {
+    if (typeof (item) !== 'string') return null;
     if (item.startsWith(startString) && startString.length > 0) {
       strings.push(item.slice(startString.length, item.length));
     }
