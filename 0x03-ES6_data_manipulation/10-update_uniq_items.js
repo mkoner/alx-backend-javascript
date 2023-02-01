b2f424935ece5d64e3ba8d4de8f529bf561a2f45
mkoner <amzid@hotmail.fr>
2023-02-01 00:20:31 +0000

    Task10 I updated to 10 instead of 100

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(items) {
   if (!(items instanceof Map)) throw new Error('Cannot process');
   for (const [key, value] of items) {
     if (value === 1) {
-      items.set(key, 10);
+      items.set(key, 100);
     }
   }
 
