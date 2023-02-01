81ee861ffaea8549e2d641eda24f389ac9f518f6
mkoner <amzid@hotmail.fr>
2023-02-01 00:18:12 +0000

    Task10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(items) {
+  if (!(items instanceof Map)) throw new Error('Cannot process');
+  for (const [key, value] of items) {
+    if (value === 1) {
+      items.set(key, 10);
+    }
+  }
+
+  return items;
+}
