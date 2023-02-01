7d15e3d93387625b1e13a5c1dd9bdad1bf5b4611
mkoner <amzid@hotmail.fr>
2023-02-01 16:03:19 +0000

    Task3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,7 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number,
 }
+
+function printTeacher(firstName: string, lastName: string): String {
+  return `${firstName.slice(0,1)}. ${lastName}}`;
+}
