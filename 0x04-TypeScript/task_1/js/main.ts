281b59907698a98a080812ad10a86ed69b66a18c
mkoner <amzid@hotmail.fr>
2023-02-01 16:31:37 +0000

    Task4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,3 +14,33 @@ interface Directors extends Teacher {
 function printTeacher(firstName: string, lastName: string): String {
   return `${firstName.slice(0,1)}. ${lastName}}`;
 }
+
+interface ConstructorInterface {
+  firstName: string,
+  lastName: string,
+}
+
+interface ClassInterface {
+  firstName: string,
+  lastName: string,
+  workOnHomework: () => string,
+  displayName: () => string,
+}
+
+class StudentClass implements ClassInterface{
+  firstName: string;
+  lastName: string;
+
+  constructor(constructor: ConstructorInterface) {
+    this.firstName = constructor.firstName;
+    this.lastName = constructor.lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
