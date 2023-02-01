8d28d6e7befe571ef62f23009ad59d478a48c822
mkoner <amzid@hotmail.fr>
2023-02-01 15:50:38 +0000

    Task2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,12 @@
 interface Teacher {
-    readonly firstName: string,
-    readonly lastName: string,
-    fullTimeEmployee: boolean,
-    yearsOfExperience?: number,
-    location: string,
-    [propName: string]: any,
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+  [propName: string]: any,
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number,
 }
