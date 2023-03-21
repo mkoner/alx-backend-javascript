caaf87fc566af5bf3db69dcf33240fe42ab7a28e
mkoner <amzid@hotmail.fr>
2023-03-21 13:10:59 +0000

    Task2 again

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -14,29 +14,33 @@ const printStudents = (students) => {
 const countStudents = (path) => {
   const fields = {};
 
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const students = data.split('\n');
-    students.shift();
-    console.log(`Number of students: ${students.length}`);
-    students.forEach((student) => {
-      const field = student.split(',')[3].trim();
-      if (field in fields) {
-        fields[`${field}`].push(student.split(',')[0]);
-      } else {
-        fields[`${field}`] = [];
-        fields[`${field}`].push(student.split(',')[0]);
-      }
-    });
-
-    for (const field in fields) {
-      if (fields[`${field}`]) {
-        process.stdout.write(`Number of students in ${field}: ${fields[`${field}`].length}. List: `);
-        printStudents(fields[`${field}`]);
-      }
-    }
-  } catch (err) {
+  if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
+  }
+
+  if (!fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+
+  const data = fs.readFileSync(path, 'utf8');
+  const students = data.split('\n');
+  students.shift();
+  console.log(`Number of students: ${students.length}`);
+  students.forEach((student) => {
+    const field = student.split(',')[3].trim();
+    if (field in fields) {
+      fields[`${field}`].push(student.split(',')[0]);
+    } else {
+      fields[`${field}`] = [];
+      fields[`${field}`].push(student.split(',')[0]);
+    }
+  });
+
+  for (const field in fields) {
+    if (fields[`${field}`]) {
+      process.stdout.write(`Number of students in ${field}: ${fields[`${field}`].length}. List: `);
+      printStudents(fields[`${field}`]);
+    }
   }
 };
 
