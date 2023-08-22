de58abe5edca48dfe9555d0142979527d54e56f8
mkoner <amzid@hotmail.fr>
2023-08-22 13:15:08 +0000

    2-read_file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const countStudents = (path) => {
   const fields = {};
+  let total = 0;
 
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
@@ -14,10 +15,10 @@ const countStudents = (path) => {
   const data = fs.readFileSync(path, 'utf8');
   const students = data.split('\n');
   students.shift();
-  console.log(`Number of students: ${students.length}`);
   students.forEach((student) => {
     if (student.split(',').length === 4) {
       const field = student.split(',')[3].trim();
+      total += 1;
       if (field in fields) {
         fields[`${field}`] += `, ${student.split(',')[0]}`;
       } else {
@@ -26,6 +27,7 @@ const countStudents = (path) => {
       }
     }
   });
+  console.log(`Number of students: ${total}`);
   for (const field in fields) {
     if (fields[`${field}`]) {
       console.log(`Number of students in ${field}: ${fields[`${field}`].split(',').length}. List: ${fields[`${field}`]}`);
