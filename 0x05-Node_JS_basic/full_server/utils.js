97b0db37961bc231e34be02e53273710a5755fe1
mkoner <amzid@hotmail.fr>
2023-08-22 18:37:51 +0000

    Added full_serveur/utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,28 +1,29 @@
 const fs = require('fs');
 
-const readDatabase = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf-8', (err, data) => {
-    const fields = {};
-    if (err) {
-      reject(new Error('Cannot load the database'));
-    }
-    if (data) {
-      const students = data.split('\n');
-      students.shift();
-      students.forEach((student) => {
-        if (student.split(',').length === 4) {
-          const field = student.split(',')[3].trim();
-          if (field in fields) {
-            fields[`${field}`].push(`${student.split(',')[0]}`);
-          } else {
-            fields[`${field}`] = [];
-            fields[`${field}`].push(`${student.split(',')[0]}`);
-          }
+async function readDatabase(path) {
+  let data;
+  const fields = {};
+  try {
+    data = await fs.promises.readFile(path, 'utf8');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+  if (data) {
+    const students = data.split('\n');
+    students.shift();
+    students.forEach((student) => {
+      if (student.split(',').length === 4) {
+        const field = student.split(',')[3].trim();
+        if (field in fields) {
+          fields[`${field}`].push(`${student.split(',')[0]}`);
+        } else {
+          fields[`${field}`] = [];
+          fields[`${field}`].push(`${student.split(',')[0]}`);
         }
-      });
-      resolve(fields);
-    }
-  });
-});
+      }
+    });
+  }
+  return fields;
+}
 
 module.exports = readDatabase;
