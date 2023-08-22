04f36db81f0369b2ef3e24f6ccdaefe0631d3bd4
mkoner <amzid@hotmail.fr>
2023-08-22 16:15:22 +0000

    Tak5 and tak3

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,60 +1,27 @@
 const http = require('http');
-const fs = require('fs');
+const students = require('./3-read_file_async');
 
+const hostname = '127.0.0.1';
 const port = 1245;
 
-const countStudents = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf-8', (err, data) => {
-    const fields = {};
-    let total = 0;
-    if (err) {
-      reject(new Error('Cannot load the database'));
-    }
-    if (data) {
-      const students = data.split('\n');
-      students.shift();
-      students.forEach((student) => {
-        if (student.split(',').length === 4) {
-          const field = student.split(',')[3].trim();
-          total += 1;
-          if (field in fields) {
-            fields[`${field}`] += `, ${student.split(',')[0]}`;
-          } else {
-            fields[`${field}`] = '';
-            fields[`${field}`] += `${student.split(',')[0]}`;
-          }
-        }
-      });
-      resolve({ total, fields });
-    }
-  });
-});
-
-const app = http.createServer(async (req, res) => {
-  const db = process.argv[2] ? process.argv[2] : '';
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
-  }
-  if (req.url === '/students') {
-    let content = '';
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    const data = await countStudents(db);
-    content += 'This is the list of our students\n';
-    content += `Number of students: ${data.total} \n`;
-    for (const field in data.fields) {
-      if (data.fields[`${field}`]) {
-        content += `Number of students in ${field}: ${data.fields[`${field}`].split(',').length}. List: ${data.fields[`${field}`]} \n`;
-      }
-    }
-    res.end(content.trim());
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    students(process.argv[2]).then((data) => {
+      res.write(`Number of students: ${data.students.length}\n`);
+      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+      res.end();
+    }).catch((err) => res.end(err.message));
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running at port ${port}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
 module.exports = app;
