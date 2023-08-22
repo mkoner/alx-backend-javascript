882b94199152431c22d80d1e84ccd903e6399cb7
mkoner <amzid@hotmail.fr>
2023-08-22 17:24:20 +0000

    Task5 another fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,25 +4,25 @@ const students = require('./3-read_file_async');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    /* const data = await students(db);
     res.write('This is the list of our students\n');
-    const data = await students(process.argv[2]);
     res.write(`Number of students: ${data.total}\n`);
-    res.write(`Number of students in CS: ${data.fields.CS.split(',').length}. List: ${data.fields.CS}\n`);
-    res.write(`Number of students in SWE: ${data.fields.SWE.split(',').length}. List: ${data.fields.SWE}`);
-    res.end();
-    /* students(process.argv[2]).then((data) => {
-      res.write('This is the list of our students\n');
+    res.write(`Number of students in CS: ${data.fields.CS.length}. List: ${data.fields.CS}\n`);
+    res.write(`Number of students in SWE: ${data.fields.SWE.length}. List: ${data.fields.SWE}`);
+    res.end(); */
+    res.write('This is the list of our students\n');
+    students(process.argv[2]).then((data) => {
       res.write(`Number of students: ${data.total}\n`);
       res.write(`Number of students in CS: ${data.fields.CS.split(',').length}. List: ${data.fields.CS}\n`);
       res.write(`Number of students in SWE: ${data.fields.SWE.split(',').length}. List: ${data.fields.SWE}`);
       res.end();
-    }).catch((err) => res.end(err.message)); */
+    }).catch((err) => res.end(err.message));
   }
 });
 
