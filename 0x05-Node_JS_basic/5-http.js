b0704ed835e82f32b04eed33f273f38f42df4674
mkoner <amzid@hotmail.fr>
2023-08-22 17:10:21 +0000

    Task5 another fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,7 +5,6 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  const db = process.argv[2] ? process.argv[2] : '';
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
@@ -17,7 +16,7 @@ const app = http.createServer((req, res) => {
     res.write(`Number of students in CS: ${data.fields.CS.length}. List: ${data.fields.CS}\n`);
     res.write(`Number of students in SWE: ${data.fields.SWE.length}. List: ${data.fields.SWE}`);
     res.end(); */
-    students(db).then((data) => {
+    students(process.argv[2]).then((data) => {
       res.write('This is the list of our students\n');
       res.write(`Number of students: ${data.total}\n`);
       res.write(`Number of students in CS: ${data.fields.CS.length}. List: ${data.fields.CS}\n`);
