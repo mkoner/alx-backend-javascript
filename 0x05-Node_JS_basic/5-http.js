4bda5ab1d268961a7ec3847f980cf380a99ccc85
mkoner <amzid@hotmail.fr>
2023-08-22 16:40:57 +0000

    Update for tak5 again

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,19 +4,23 @@ const students = require('./3-read_file_async');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
+  const db = process.argv[2] ? process.argv[2] : '';
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    students(process.argv[2]).then((data) => {
-      res.write(`Number of students: ${data.students.length}\n`);
-      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-      res.end();
-    }).catch((err) => res.end(err.message));
+    let content = '';
+    const data = await students(db);
+    content += 'This is the list of our students\n';
+    content += `Number of students: ${data.total} \n`;
+    for (const field in data.fields) {
+      if (data.fields[`${field}`]) {
+        content += `Number of students in ${field}: ${data.fields[`${field}`].split(',').length}. List: ${data.fields[`${field}`]} \n`;
+      }
+    }
+    res.end(content.trim());
   }
 });
 
