940cf38e98ce504e63e87d451d1f6359aaf3f99c
mkoner <amzid@hotmail.fr>
2023-08-22 19:26:49 +0000

    Full server

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,37 +1,35 @@
 const readDatabase = require('../utils');
 
-export default class StudentsController {
+class StudentsController {
   static getAllStudents(request, response) {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
     response.write('This is the list of our students\n');
-    readDatabase('database.csv').then((data) => {
-      for (const key in data) {
-        if (data[`${key}`]) {
-          response.write(`Number of students in ${key}: ${data[`${key}`].length}. List: ${data[`${key}`].join(', ')} \n`);
-        }
-      }
-      response.status(200).end();
-    }).catch((error) => {
-      //response.sendStatus(500);
-      response.write(error.message);
-      response.status(500).end();
-    });
+    readDatabase('./database.csv').then((data) => {
+      response.write(`Number of students in CS: ${data.CS.length}. List: ${data.CS.join(', ')}\n`);
+      response.write(`Number of students in SWE: ${data.SWE.length}. List: ${data.SWE.join(', ')}\n`);
+      response.end();
+    }).catch((err) => res.write(err.message))
+      .finally(() => {
+        res.end();
+      });
   }
 
   static getAllStudentsByMajor(request, response) {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
     const { major } = request.params;
     if (major !== 'CS' && major !== 'SWE') {
-      //response.sendStatus(200);
-      response.write('Major parameter must be CS or SWE');
-      response.status(500).end();
+      response.statusCode = 500;
+      response.write('Major parameter must be CS or SWE\n');
+      response.end();
+      return;
     }
-    readDatabase('database.csv').then((data) => {
-      //response.sendStatus(200);
-      response.write(`List: ${data[`${major}`].join(', ')} \n`);
-      response.status(200).end();
-    }).catch((error) => {
-      ////response.sendStatus(500);
-      response.write(error.message);
-      response.status(500).end();
-    });
+    readDatabase('./database.csv').then((data) => {
+      response.write(`List: ${data[major].join(', ')}\n`);
+      response.end();
+    }).catch((err) => response.send(err.message));
   }
 }
+
+export default StudentsController;
