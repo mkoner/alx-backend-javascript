0bcae7246dcffaacbcb3a83605cde215d40f1d68
mkoner <amzid@hotmail.fr>
2023-08-27 00:14:57 +0000

    Task3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -6,11 +6,11 @@ const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
     it('Should make sure that sendPaymentRequestToApi uses Utils.calculateNumber', () => {
-        const utilSpy = sinon.spy(Utils);
+        const utilSpy = sinon.spy(Utils, 'calculateNumber');
         sendPaymentRequestToApi(100, 20);
         //utilSpy.calculateNumber('SUM', 100, 20)
-        expect(utilSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-        expect(utilSpy.calculateNumber.callCount).to.equal(1);
-        utilSpy.calculateNumber.restore();
+        expect(utilSpy.calledWith('SUM', 100, 20)).to.be.true;
+        expect(utilSpy.calledOnce).to.be.true;
+        utilSpy.restore();
     });
 }); 
