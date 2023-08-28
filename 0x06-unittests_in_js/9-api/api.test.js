4713f203cc1de3a847ae0dc2c5ce1bb766beadd9
mkoner <amzid@hotmail.fr>
2023-08-28 16:51:31 +0000

    Task9

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,45 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+    const API_URL = 'http://localhost:7865';
+    it('Should test the status code and content of body', (done) => {
+        const options = {
+            url: API_URL,
+            method: 'GET',
+        };
+        request(options, (err, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+});
+
+describe('The cart page', () => {
+    const API_URL = 'http://localhost:7865';
+    it('Should test the status code and content of body for a number', (done) => {
+        const id = 78;
+        const options = {
+            url: `${API_URL}/cart/${id}`,
+            method: 'GET',
+        };
+        request(options, (err, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal(`Payment methods for cart ${id}`);
+            done();
+        });
+    });
+
+    it('Should test the incorrect status', (done) => {
+        const id = 'hello';
+        const options = {
+            url: `${API_URL}/cart/${id}`,
+            method: 'GET',
+        };
+        request(options, (err, response, body) => {
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
+    });
+});
